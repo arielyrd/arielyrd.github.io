e6a7882de1651aec964fe3a54cc4f2e0c798a7fb
arielyrd <arielyordan2004@gmail.com>
2025-01-20 16:55:27 +0700

    nambahin link web personal

--- src/components/Home/Resume.tsx
+++ src/components/Home/Resume.tsx
@@ -25,7 +25,7 @@ const Resume = () => {
           <div className="w-20 h-1 bg-gradient-to-r from-accent-primary to-accent-secondary mx-auto mb-8"></div>
 
           <motion.a
-            href="https://drive.google.com/file/d/1a7TWX1_r2l4YeOVQqD1ox4_mEBZZox5X/view?usp=drive_link"
+            href="https://drive.google.com/file/d/19Gmt8m04b7Zm8wKrmwdKavAisqcSYlDh/view?usp=drive_link"
             download
             className="inline-flex items-center gap-2 bg-gradient-to-r from-accent-primary to-accent-secondary px-8 py-4 rounded-full text-white group hover:shadow-lg hover:shadow-accent-primary/25 transition-all duration-300"
             whileHover={{ scale: 1.05 }}
