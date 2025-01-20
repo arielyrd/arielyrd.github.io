787c8bf69167920106390002ff32ccf073472df0
arielyrd <arielyordan2004@gmail.com>
2025-01-20 14:18:48 +0700

    ubah path di index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["lucide-react"], // Opsi ini tetap dipertahankan
-  },
+  base: "/arielyrd.github.io/", // Sesuaikan dengan nama repositori GitHub Anda
   server: {
     host: "0.0.0.0", // Untuk development di localhost
     port: 5173, // Port default Vite
   },
-  base: "/", // Base URL untuk GitHub Pages (root domain)
 });
