dcc18c7b93df86b53250dfd60c88c4f7220e8d64
arielyrd <arielyordan2004@gmail.com>
2025-01-20 14:29:40 +0700

    balik awal

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/arielyrd.github.io/", // Sesuaikan dengan nama repositori GitHub Anda
-  server: {
-    host: "0.0.0.0", // Untuk development di localhost
-    port: 5173, // Port default Vite
+  optimizeDeps: {
+    exclude: ["lucide-react"],
   },
 });
